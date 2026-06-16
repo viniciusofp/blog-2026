c2784dae75de529058cb9046b2102f43a0a6553d
viniciusofp <viniciusofp@gmail.com>
2026-06-16 13:12:27 -0300

    photo inside public dir

--- src/components/blog/BlogHeader.tsx
+++ src/components/blog/BlogHeader.tsx
@@ -3,6 +3,8 @@ import { getPayload } from "payload";
 
 import config from "@/payload.config";
 import { CustomRichText } from "@/components/CustomRichText";
+import me from "@/public/me.jpg";
+
 const payloadConfig = await config;
 const payload = await getPayload({ config: payloadConfig });
 
@@ -15,7 +17,7 @@ export default async function BlogHeader(props: BlogHeaderProps) {
     <div className="mb-8 flex items-center gap-4">
       <div className="size-14 shrink-0 overflow-hidden rounded-full border-2 border-white">
         <img
-          src="https://media.licdn.com/dms/image/v2/D4D03AQFZElajahjxsA/profile-displayphoto-crop_800_800/B4DZg9PsAIHYAM-/0/1753374185609?e=1782950400&v=beta&t=JP98wa2L4adDSYUfIMFr9xDJ9ESyyBLKW2eKrlh8xNk"
+          src={me.src}
           alt=""
           className="h-full w-full object-cover object-center"
         />
