a05877c0fcdb61d1b3715e64eccb2c8a3ebdfb36
viniciusofp <viniciusofp@gmail.com>
2026-07-06 14:21:36 -0300

    remove readOnly from "isApproved" @ Comments collection

--- src/collections/Comments.ts
+++ src/collections/Comments.ts
@@ -39,7 +39,6 @@ export const Comments: CollectionConfig = {
       type: "checkbox",
       label: "Aprovar comentário",
       defaultValue: false,
-      admin: { readOnly: true },
     },
     {
       name: "acceptsEmail",
