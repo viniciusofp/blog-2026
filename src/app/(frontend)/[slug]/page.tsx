8366fc60c1365dcbef16a7d7882404375671ee3d
viniciusofp <viniciusofp@gmail.com>
2026-07-07 14:00:40 -0300

    fix selection

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -177,7 +177,7 @@ export default async function BlogPost({
       <div className="">
         <CustomRichText
           data={post.content}
-          className="prose-headings:font-bold prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl lg:prose-h1:text-5xl lg:prose-h2:text-4xl lg:prose-h3:text-3xl lg:prose-h4:text-2xl prose prose-lg lg:prose-2xl prose-a:underline prose-a:decoration-[#FFF7B1] prose-a:[text-decoration-skip-ink:none] prose-a:underline-offset-[-0.25em] prose-a:hover:underline prose-a:decoration-[0.5em] prose-a:hover:decoration-[#FFEE50] text-pretty selection:underline selection:decoration-orange-600/20"
+          className="prose-headings:font-bold prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl lg:prose-h1:text-5xl lg:prose-h2:text-4xl lg:prose-h3:text-3xl lg:prose-h4:text-2xl prose prose-lg lg:prose-2xl prose-a:underline prose-a:decoration-[#FFF7B1] prose-a:[text-decoration-skip-ink:none] prose-a:underline-offset-[-0.25em] prose-a:hover:underline prose-a:decoration-[0.5em] prose-a:hover:decoration-[#FFEE50] text-pretty selection:bg-orange-300 selection:text-orange-900"
         />
       </div>
       {post.updatedAt !== post.createdAt ? (
